d8886d1fc397df63fdaa3283feef64d8db5914a6
Imran Kabir <ikr22015@gmail.com>
2018-01-24 14:24:42 -0500

    install cloud env on my framework.

--- HealthFIrst/src/main/java/pageobject/HomePage.java
+++ HealthFIrst/src/main/java/pageobject/HomePage.java
@@ -1,14 +1,17 @@
 package pageobject;
 
+import base.CommonAPI;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage {
+public class HomePage extends CommonAPI{
     @FindBy(css = "input.display.search_box_upd")
     WebElement searchBox;
 
     @FindBy(xpath = "//input[@name = 'sa']")
     WebElement goBtn;
+
+
 
     public void search(){
         searchBox.sendKeys("doctor");
